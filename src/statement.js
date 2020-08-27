e690adb823c1f867279bbe1676a692c8e063ccd0
DolngNothing <1178363215@qq.com>
2020-08-27 15:31:18 +0800

    [refactor-exe] [christopher] refactor: extract render

--- src/statement.js
+++ src/statement.js
@@ -28,36 +28,45 @@ formatUSD = (thisAmount) => {
   }).format(thisAmount / 100)
 }
 
-caculateCredit = (invoice,plays) => {
-  let totalCredit=0;
-  for(let perf of invoice.performances){
-    let credit=0;
+caculateTotalAmount = (invoice, plays) => {
+  let total = 0;
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    total += caculateAmount(perf.audience, play);
+  }
+  return total;
+}
+
+caculateCredit = (invoice, plays) => {
+  let totalCredit = 0;
+  for (let perf of invoice.performances) {
+    let credit = 0;
     const play = plays[perf.playID];
     credit += Math.max(perf.audience - 30, 0);
-      // add extra credit for every ten comedy attendees
-      if ('comedy' === play.type) credit += Math.floor(perf.audience / 5);
+    // add extra credit for every ten comedy attendees
+    if ('comedy' === play.type) credit += Math.floor(perf.audience / 5);
 
-      totalCredit+=credit;
+    totalCredit += credit;
   }
   return totalCredit
 }
 
-renderEnding = (result,totalAmount,volumeCredits) => {
-  result += `Amount owed is ${formatUSD(totalAmount)}\n`;
+renderEnding = (totalAmount, volumeCredits) => {
+  let result = `Amount owed is ${formatUSD(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits \n`;
   return result;
 }
 
+renderInfo = (audience,play) => {
+  return ` ${play.name}: ${formatUSD(caculateAmount(audience, play))} (${audience} seats)\n`;
+}
+
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    thisAmount = caculateAmount(perf.audience, play);
-    totalAmount += thisAmount;
-    result += ` ${play.name}: ${formatUSD(thisAmount)} (${perf.audience} seats)\n`;
+    result += renderInfo(perf.audience,plays[perf.playID])
   }
-  return renderEnding(result,totalAmount,caculateCredit(invoice, plays));
+  return result += renderEnding(caculateTotalAmount(invoice, plays), caculateCredit(invoice, plays));
 }
 
 
