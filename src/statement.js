3e26dbf96c92b2fa3c16bc7adac67ba0412a1263
DolngNothing <1178363215@qq.com>
2020-08-27 14:47:15 +0800

    [refactor-exe] [christopher] refactor: extract caculate thisAmount and switch case

--- src/statement.js
+++ src/statement.js
@@ -1,4 +1,27 @@
-function statement (invoice, plays) {
+caculateAmount = (audience, play) => {
+  let thisAmount = 0;
+  switch (play.type) {
+    case 'tragedy':
+      thisAmount = 40000;
+      if (audience > 30) {
+        thisAmount += 1000 * (audience - 30);
+      }
+      break;
+    case 'comedy':
+      thisAmount = 30000;
+      if (audience > 20) {
+        thisAmount += 10000 + 500 * (audience - 20);
+      }
+      thisAmount += 300 * audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+  return thisAmount;
+}
+
+
+function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
@@ -9,31 +32,15 @@ function statement (invoice, plays) {
   }).format;
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    thisAmount=caculateAmount(perf.audience,play);
+    totalAmount += thisAmount;
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
     if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
     //print line for this order
     result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits \n`;
